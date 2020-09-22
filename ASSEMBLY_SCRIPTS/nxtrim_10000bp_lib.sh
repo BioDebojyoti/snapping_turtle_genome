d21f4ea30e138370a6f4b84b854edaebc12f26be
turkrhen <69688935+turkrhen@users.noreply.github.com>
2020-09-22 17:49:42 -0500

    Delete nxtrim_10000bp_lib.sh

--- ASSEMBLY_SCRIPTS/nxtrim_10000bp_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-t1=$(date +%s)
-
-nxtrim --rf --separate --aggressive --minlength 25 --output-prefix 10kb_lib -1 /Volumes/G-SPEED_Shuttle_TB3/GENOME_SEQ_HCI/RAW/13482X2_161222_D00294_0282_BCAB4VANXX_7_1.fq.gz -2 /Volumes/G-SPEED_Shuttle_TB3/GENOME_SEQ_HCI/RAW/13482X2_161222_D00294_0282_BCAB4VANXX_7_2.fq.gz
-
-t2=$(date +%s)
-echo "execution time"$((t2 -t1))
